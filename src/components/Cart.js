cdd8753ccd0c1222ec6c15ff669084e3b57ddfda
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-12-22 15:10:40 +0100

    first commit

---
+++ src/components/Cart.js
@@ -0,0 +1,40 @@
+import { memo } from "react";
+
+const Cart = ({ cart, handleRemoveFromCart, handleAddToCart, total }) => {
+  console.log("rendre cart");
+  return cart.length !== 0 ? (
+    <div className="cart">
+      {cart.map((meal) => {
+        return (
+          <div key={meal.id}>
+            <div>
+              <button
+                onClick={() => {
+                  handleRemoveFromCart(meal);
+                }}
+              >
+                -
+              </button>
+              <span>{meal.quantity}</span>
+              <button
+                onClick={() => {
+                  handleAddToCart(meal);
+                }}
+              >
+                +
+              </button>
+            </div>
+
+            <span>{meal.title}</span>
+            <span>{(meal.price * meal.quantity).toFixed(2)} €</span>
+          </div>
+        );
+      })}
+      <p> TOTAL : {total.toFixed(2)}</p>
+    </div>
+  ) : (
+    <div>Panier vide</div>
+  );
+};
+
+export default memo(Cart);
---
+++ src/components/Category.js
@@ -0,0 +1,19 @@
+import Meal from "./Meal";
+
+const Category = ({ category, handleAddToCart }) => {
+  console.log("rendre category");
+  return (
+    <div className="category">
+      <h2>{category.name}</h2>
+      <div className="meals-container">
+        {category.meals.map((meal) => {
+          return (
+            <Meal key={meal.id} meal={meal} handleAddToCart={handleAddToCart} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
